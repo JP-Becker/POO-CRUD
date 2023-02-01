23e3811e9688ffe23c42c496906e469fb6f89bf0
JP-Becker <jpbecker20@gmail.com>
2023-02-01 19:31:25 -0300

    classe e controller feitos

---
+++ src/controller/SongController.ts
@@ -0,0 +1,27 @@
+import { SongDB } from "../types";
+import { BaseDatabase } from "../database/BaseDatabase";
+
+export class SongDatabase extends BaseDatabase {
+    public static TABLE_SONGS = "songs"
+
+    public async finSongs() {
+        const songDB: SongDB[] = await BaseDatabase
+            .connection(SongDatabase.TABLE_SONGS)
+
+        return songDB
+    }
+
+    public async findSongById(id: string) {
+        const [ songDB ]: SongDB[] | undefined[] = await BaseDatabase
+            .connection(SongDatabase.TABLE_SONGS)
+            .where({ id })
+
+        return songDB
+    }
+
+    public async insertSong(newSongDB: SongDB) {
+        await BaseDatabase
+            .connection(SongDatabase.TABLE_SONGS)
+            .insert(newSongDB)
+    }
+}

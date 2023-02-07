2bc7a9a3de7157457caac0d33422f0cbcfc89825
JP-Becker <jpbecker20@gmail.com>
2023-02-07 14:22:47 -0300

    endpoints refatorados para o uso do router

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { SongController } from "./controller/SongController";
+import { songRouter } from "./router/songRouter";
 
 
 const app = express();
@@ -14,38 +15,35 @@ app.listen(3003, () => {
 
 
 // Endpoint para testar a conexao com o banco de dados
-app.get("/ping", async (req: Request, res: Response) => {
-  try {
-    res.status(200).send({ message: "Pong!" });
-  } catch (error) {
-    console.log(error);
+// app.get("/ping", async (req: Request, res: Response) => {
+//   try {
+//     res.status(200).send({ message: "Pong!" });
+//   } catch (error) {
+//     console.log(error);
 
-    if (req.statusCode === 200) {
-      res.status(500);
-    }
+//     if (req.statusCode === 200) {
+//       res.status(500);
+//     }
 
-    if (error instanceof Error) {
-      res.send(error.message);
-    } else {
-      res.send("Erro inesperado");
-    }
-  }
-});
+//     if (error instanceof Error) {
+//       res.send(error.message);
+//     } else {
+//       res.send("Erro inesperado");
+//     }
+//   }
+// });
 
+
+// // retorna a tabela de músicas (songs)
+// app.get("/songs", songController.getSongs)
+// // endpoint para criar uma música
+// app.post("/songs", songController.createSong)
+// // editar uma música buscada por ID
+// // apenas "artist", "name" e "total_views" são obrigatórios no body
+// app.put("/songs/:id", songController.editSong)
+// // deletar uma música da tabela por ID
+// app.delete("/songs/:id", songController.deleteSong)
 
 const songController = new SongController() // instanciando a classe songController que contém as funções assíncronas dos enpoints
-// retorna a tabela de músicas (songs)
-app.get("/songs", songController.getSongs)
 
-
-// endpoint para criar uma música
-app.post("/songs", songController.createSong)
-
-
-// editar uma música buscada por ID
-// apenas "artist", "name" e "total_views" são obrigatórios no body
-app.put("/songs/:id", songController.editSong)
-
-
-// deletar uma música da tabela por ID
-app.delete("/songs/:id", songController.deleteSong)
+app.use("/songs", songRouter)
